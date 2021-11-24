c93e9f74efbba423023c3e3f490fd81aa271f9aa
ThoBT <thob2105@gmail.com>
2021-11-24 22:43:01 +0700

    create migrate for category model

---
+++ migrations/migrate-create-category.js
@@ -0,0 +1,31 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize
+      .query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";')
+      .then(() => {
+        return queryInterface.createTable('Categories', {
+          id: {
+            allowNull: false,
+            primaryKey: true,
+            type: Sequelize.UUID,
+            defaultValue: Sequelize.literal('uuid_generate_v4()'),
+          },
+          name: {
+            allowNull: false,
+            type: Sequelize.STRING(50),
+          },
+          created_at: {
+            allowNull: false,
+            type: Sequelize.DATE,
+          },
+          updated_at: {
+            allowNull: false,
+            type: Sequelize.DATE,
+          },
+        });
+      });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Categories');
+  },
+};
---
+++ models/category.js
@@ -0,0 +1,34 @@
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Category extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Category.init(
+    {
+      name: DataTypes.STRING(50),
+      createdAt: {
+        type: DataTypes.DATE,
+        field: 'created_at',
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        field: 'updated_at',
+      },
+    },
+    {
+      sequelize,
+      timestamps: true,
+      tableName: 'Categories',
+      modelName: 'Category',
+    }
+  );
+  return Category;
+};
